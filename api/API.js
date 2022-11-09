9ebe5b1d14d56656245aca4cdc2ca3f4eb0e1498
jayant_kumar_baghwar_fw22_0991 <jayantjay389@gmail.com>
2022-11-09 23:41:19 +0530

    Home page and sign in login added

---
+++ api/API.js
@@ -0,0 +1,314 @@
+
+let arr  = [
+    {   id:1,
+       name : "BLANKNYC",
+       img : "https://n.nordstrommedia.com/id/sr3/5f0d72c3-b963-4a44-b4c0-1127df2c5f31.jpeg?h=365&w=240&dpr=2",
+       category : "dress",
+       for : "women",
+       price : 8427,
+       description : 'Faux Suede Flare Leg Pants',
+       rating : 3.9,
+       age : 'A'
+
+    },
+    {   
+        id :2,
+        name : "Wit & Wisdom",
+        img : "https://n.nordstrommedia.com/id/sr3/f6d27119-baca-4d91-973c-eca527adb6c3.jpeg?h=365&w=240&dpr=2",
+        category : "dress",
+        for : "women",
+        price : 6427,
+        description : 'Ab-Solution High Waist Itty Bitty Bootcut Jeans (Regular & Petite) (Nordstrom Exclusive)',
+        rating : 4,
+        age : 'A'
+    },
+    {
+        id :3,
+        name : "CeCe",
+        img : "https://n.nordstrommedia.com/id/sr3/cfb687f9-ff6d-455b-b8f9-9a109014ab60.jpeg?h=365&w=240&dpr=2",
+        category : "dress",
+        for : "women",
+        price : 600,
+        description : 'Clip Dot Sleeve Sweater',
+        rating : 3.8,
+        age : 'A'
+    },
+    {
+        id :4,
+        name : "Wit & Wisdom",
+        img : "https://n.nordstrommedia.com/id/sr3/144335f8-78f0-48d5-b56b-356e9bb6c0c2.jpeg?h=365&w=240&dpr=2",
+        category : "dress",
+        for : "women",
+        price : 6427,
+        description : 'Puff Sleeve Crewneck Sweater',
+        rating : 3,
+        age : 'A'
+    },
+    {
+        id :5,
+        name : "Marc Fisher LTD",
+        img : "https://n.nordstrommedia.com/id/sr3/e8ad26aa-d31b-4161-b26a-c4012aea1dfe.jpeg?h=365&w=240&dpr=2",
+        category : "shoes",
+        for : "women",
+        price : 3999,
+        description : 'Zala Block Heel Pump (Women)',
+        rating : 3.1,
+        age : 'A'
+    },
+    {
+        id :6,
+        name : "Naturala",
+        img : "https://n.nordstrommedia.com/id/sr3/8998c32b-86f6-4db1-a571-f3a402c555fb.jpeg?h=365&w=240&dpr=2",
+        category : "shoes",
+        for : "women",
+        price : 5999,
+        description : 'True Colors Vera Ankle Strap Sandal (Women)',
+        rating : 4.1,
+        age : 'A'
+    },
+    {
+        id :7,
+        name : "cendra scot",
+        img : "https://th.bing.com/th/id/OIP.2ljYsetgr-AkoTIMXLk9xgHaJ4?w=203&h=271&c=7&r=0&o=5&dpr=1.3&pid=1.7",
+        category : "jewelry",
+        for : "women",
+        price : 1999,
+        description : 'Kendall Stone Pendant Necklace',
+        rating : 4.5,
+        age : 'A'
+    },
+    {
+        id :8,
+        name : "babulibar",
+        img : "https://th.bing.com/th/id/OIP.5Rirx3bnqF4nNl3Ov3-VjgHaK1?w=203&h=298&c=7&r=0&o=5&dpr=1.3&pid=1.7",
+        category : "jewelry",
+        for : "women",
+        price : 2599,
+        description : 'pavé mini initial pendant necklace',
+        rating : 2.9,
+        age : 'A'
+    },
+    {
+        id :9,
+        name : "pentu",
+        img : "https://rukminim1.flixcart.com/image/556/667/l0pm3680/kids-dress/h/b/f/5-6-years-dg001-smart-bazar-original-imagcf3rpn8hskza.jpeg?q=50",
+        category : "dress",
+        for : "women",
+        price : 2699,
+        description : 'Kids Kids Stretch Cotton Moto Leggings (Toddler Girls, Little Girls & Big Girls)',
+        rating : 3.3,
+        age : 'k'
+    },
+    {
+        id :10,
+        name : "Nike",
+        img : "https://rukminim1.flixcart.com/image/556/667/ky3b0y80/kids-dress/7/3/o/9-10-years-gf0092-teal-fashion-dream-original-imagaeh3zcg8gpxm.jpeg?q=50",
+        category : "dress",
+        for : "women",
+        price : 1699,
+        description : 'Kids French Terry Shorts (Little Boy & Big Boy)',
+        rating : 3.1,
+        age : 'k'
+    },
+    {
+        id :11,
+        name : "Kurat",
+        img : "https://rukminim1.flixcart.com/image/556/667/kflftzk0pkrrdj/kids-ethnic-set/v/w/h/6-7-years-black-digibc001-digimart-original-imafwfkatpkhgayu.jpeg?q=50",
+        category : "dress",
+        for : "men",
+        price : 2699,
+        description : 'Boys Festive & Party, Wedding Kurta, Waistcoat and Pyja',
+        rating : 3.1,
+        age : 'k'
+    },
+    {
+        id :12,
+        name : "shuit",
+        img : "https://rukminim1.flixcart.com/image/556/667/xif0q/kids-ethnic-set/j/j/n/3-4-years-whitejabla-ftc-fashions-original-imaft8pzfzegxtyb-bb.jpeg?q=50",
+        category : "dress",
+        for : "men",
+        price : 2499,
+        description : 'Boys Festive & Party Dhoti & Kurta Set',
+        rating : 3.1,
+        age : 'k'
+    },
+    {
+        id :14,
+        name : "Neska Moda",
+        img : "https://rukminim1.flixcart.com/image/556/667/k5ihzm80/kids-shoe/j/b/f/3c-bt505-loafer-neska-moda-original-imafz6h62zesugf2.jpeg?q=50",
+        category : "shoes",
+        for : "men",
+        price : 2499,
+        description : "slip on lofer boy",
+        rating : 3.1,
+        age : 'k'
+    },
+    {
+        id :15,
+        name : "Bersache",
+        img : "https://rukminim1.flixcart.com/image/556/667/l071d3k0/shopsy-shoe/o/z/i/6-ori-mm-1720-zenwear-grey-original-imagcfbh7vsh4c4v.jpeg?q=50",
+        category : "shoes",
+        for : "men",
+        price : 2499,
+        description : "lace running shoes for kid",
+        rating : 3.1,
+        age : 'k'
+    },
+    {
+        id :17,
+        name : "Dencity",
+        img : "https://rukminim1.flixcart.com/image/556/667/kxqg2a80/shoe/c/u/s/10-socks-03-aqua-10-density-aqua-original-imaga4hgckazhvg7.jpeg?q=50",
+        category : "shoes",
+        for : "men",
+        price : 2099,
+        description : "shock training & gym shoes for man",
+        rating : 3.6,
+        age : 'A'
+    },
+    {
+        id :18,
+        name : "Robbie jones",
+        img : "https://rukminim1.flixcart.com/image/309/371/kr58yvk0/shoe/q/2/j/10-95489-robbie-jones-black-original-imag5y5aham5aakz.jpeg?q=50",
+        category : "shoes",
+        for : "men",
+        price : 2177,
+        description : "Neaker for men ",
+        rating : 3.5,
+        age : 'A'
+    },
+    {
+        id :19,
+        name : "Tigones",
+        img : "https://rukminim1.flixcart.com/image/556/667/l5fnhjk0/shoe/j/y/c/7-modern-trendy-sneakers-tigonis-white-2-original-imagg44uzg94wwry.jpeg?q=50",
+        category : "shoes",
+        for : "men",
+        price : 5677,
+        description : "Neaker for fromal and casual ",
+        rating : 4.5,
+        age : 'A'
+    },
+    {
+        id :20,
+        name : "X-man",
+        img : "https://rukminim1.flixcart.com/image/556/667/k070zgw0/shirt/j/j/m/m-db-orange-x-men-original-imafkfmygdtfyxtn.jpeg?q=50",
+        category : "dress",
+        for : "men",
+        price : 5677,
+        description : "mulity color dresss",
+        rating : 4.2,
+        age : 'A'
+    },
+    {
+        id :21,
+        name : "Gigimi",
+        img : "https://rukminim1.flixcart.com/image/485/582/l41n2q80/kurta/q/i/i/s-djj17-digimi-original-imagffefzvfzhpur.jpeg?q=50",
+        category : "dress",
+        for : "men",
+        price : 5677,
+        description : "men solid Cotton shirt ",
+        rating : 4.2,
+        age : 'A'
+    },
+    {
+        id :22,
+        name : "Seventeens ",
+        img : "https://rukminim1.flixcart.com/image/556/667/xif0q/shirt/f/3/1/m-colourfull-striped-shirt-seventeenstitch-original-imaghgfr2xjawzgx.jpeg?q=50",
+        category : "dress",
+        for : "men",
+        price : 3277,
+        description : "Cool and sexy look shirt for men ",
+        rating : 4.6,
+        age : 'A'
+    },
+    {
+        id :23,
+        name : "blenz Trenz ",
+        img : "https://rukminim1.flixcart.com/image/556/667/l20rma80/trouser/c/8/z/30-black-patti-white-luxury-original-imagdg7quvhqrhhe.jpeg?q=50",
+        category : "dress",
+        for : "men",
+        price : 2033,
+        description : "slim and fit boys cotton pents ",
+        rating : 4,
+        age : 'A'
+    },
+    {
+        id :24,
+        name : "heroic ",
+        img : "https://rukminim1.flixcart.com/image/556/667/klscivk0/trouser/q/q/k/28-sktr-4242-olv-fubar-original-imagyuff8uq6cxny.jpeg?q=50",
+        category : "dress",
+        for : "men",
+        price : 2533,
+        description : "super cool fit mens pents  ",
+        rating : 3.0,
+        age : 'A'
+    },
+    {
+        id :25,
+        name : "Nivea soft",
+        img : "https://rukminim1.flixcart.com/image/612/612/kxf0jgw0/moisturizer-cream/6/g/w/-original-imag9vrgezbgaqu8.jpeg?q=70",
+        category : "skin",
+        for : "women",
+        price : 700,
+        description : "nievea Shoft moiturerizing cream all skin",
+        rating : 4.0,
+        age : 'A'
+    },
+    {
+        id :26,
+        name : "Nokolives",
+        img : "https://rukminim1.flixcart.com/image/612/612/kzx1a4w0/scrub/v/q/8/-original-imagbtrntfurxmbj.jpeg?q=70",
+        category : "skin",
+        for : "women",
+        price : 550,
+        description : "belavita nikolives for soft skins in winters",
+        rating : 4.3,
+        age : 'A'
+    },
+    {
+        id :27,
+        name : "mcaffafeine",
+        img : "https://rukminim1.flixcart.com/image/612/612/l3vxbbk0/scrub/v/a/2/100-exfoliating-coffee-body-scrub-for-tan-removal-soft-smooth-original-imagewhbuchmmswr.jpeg?q=70",
+        category : "skin",
+        for : "women",
+        price : 999,
+        description : "mcaffeine coffee body scrub for tan remobval cream",
+        rating : 3.9,
+        age : 'A'
+    },
+    {
+        id :28,
+        name : "Parachute body Lotion",
+        img : "https://rukminim1.flixcart.com/image/612/612/xif0q/moisturizer-cream/b/w/b/-original-imaggbtwtwghymz4.jpeg?q=70",
+        category : "skin",
+        for : "women",
+        price : 233,
+        description : "Parachute boby lotion for women",
+        rating : 4.4,
+        age : 'A'
+    },
+
+
+
+
+
+    
+    
+
+
+];
+
+localStorage.setItem("shopnow",JSON.stringify(arr));
+let data =JSON.parse(localStorage.getItem("shopnow"))
+
+for(let el of data){
+    let div =document.createElement('div');
+    let img =document.createElement('img')
+    img.setAttribute('src',el.img)
+    let h1 = document.createElement('h1')
+    h1.innerText = el.rating
+    div.append( h1)
+    document.querySelector('body').append(div)
+}
+
+
+ 
