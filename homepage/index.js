f39734ca30a1a5a0db2343f0338f661f21b8bfee
jayant_kumar_baghwar_fw22_0991 <jayantjay389@gmail.com>
2022-11-11 23:49:15 +0530

    Day3 Added

--- homepage/index.js
+++ homepage/index.js
@@ -4,13 +4,23 @@ Display(arr)
 Display2(arr)
 Display3(arr)
 
+function Search(){
+    let x = document.getElementById('Search').value 
+    let NewData = arr.filter(function(el){
+        return (el.category.toLowerCase().includes(x.toLowerCase())) || (el.name.toLowerCase().includes(x.toLowerCase()))
+    })
+    SearchData(NewData)
+}
+
+function SearchData(a){
+    let box = document.getElementById('card').innerHTML = ""
+    document.querySelector('.banner').innerHTML = ""
+    document.querySelector('.poster').innerHTML = ""
+    document.querySelector('.non').innerHTML = ""
+    document.getElementById('d').innerText = "Your Search Result"
 
 
-
-
-function Display(a){
-    
-    for(let i=0 ;i< 8 ;i++){
+    for(let i=0 ;i< a.length ;i++){
         // console.log(el.name)
         let div = document.createElement('div')
         let img = document.createElement('img')
@@ -31,6 +41,47 @@ function Display(a){
         
         div.append(img,name,des,price,rating)
         box.append(div)
+    }
+
+
+}
+
+
+
+
+
+let dArr = JSON.parse(localStorage.getItem("details")) || [];
+
+function Display(a){
+    let box = document.getElementById('card').innerHTML = ""
+    
+    for(let i=0 ;i< 8 ;i++){
+        // console.log(el.name)
+        let div = document.createElement('div')
+        div.addEventListener('click',function(){
+                dArr.push(a[i])
+                
+            localStorage.setItem("details",JSON.stringify(dArr))
+
+        });
+        let img = document.createElement('img')
+            img.setAttribute('src',a[i].img)
+
+        let name = document.createElement('h2')
+            name.innerText = a[i].name
+        let des = document.createElement('p')
+            des.innerText = a[i].description
+
+        let price = document.createElement('p')
+            price.innerText = a[i].price
+
+        let rating = document.createElement('p')
+            rating.innerText = a[i].rating
+    
+        let box = document.getElementById('card')
+       
+        div.append(img,name,des,price,rating)
+        box.append(div,)
     }
 }
 
